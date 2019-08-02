97b5f2e64fcdb32071b2d317aa3bc3891df7579e
Selene <selene.nijst@gmail.com>
2019-08-02 15:46:04 +0200

    Merged two versions and cleaned up some inconsistencies.

--- scripts/index.js
+++ scripts/index.js
@@ -16,8 +16,8 @@ function scrollToSection(e) {
 
 // Countdown clock functionality
 // =============================
-startCountdown(document.querySelector("#registrationTimeBlock"), new Date("Aug 25, 2019 17:00:00").getTime());
-startCountdown(document.querySelector("#startTimeBlock"), new Date("Sep 3, 2019 09:00:00").getTime());
+startCountdown(document.querySelector("#registrationTimer"), new Date("Aug 25, 2019 17:00:00").getTime());
+startCountdown(document.querySelector("#startTimer"), new Date("Sep 3, 2019 09:00:00").getTime());
 
 // Create a function for multi-countdowns on the page, ask for element to show countdown as well as the datetime to countdown to
 // =============================================================================================================================
